7a511afcc5e0b76e0ed2e0a046d48256abd6b734
william massao marubayashi saito <william.massao.ftt@gmail.com>
2020-06-20 11:54:47 -0300

    Tartaruga e Ornitorrinco

---
+++ N2_POO+ED/N2_POO+ED/Animais/Tartaruga.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2_POO_ED.Animais
+{
+    class Tartaruga : Reptil, IAquatico, IOviparo
+    {
+
+        public Tartaruga()
+        {
+            TemEscamas = true;
+            TemCasco = true;
+        }
+
+        public bool ViveEmTerra
+        {
+            get
+            {
+                return ViveEmTerra;
+            }
+
+            set
+            {
+                ViveEmTerra = false;
+            }
+        }
+        public bool Mergulho
+        {
+            get
+            {
+                return Mergulho;
+            }
+            set
+            {
+                Mergulho = true;
+            }
+        }
+
+        public bool AguaDoce
+        {
+            get
+            {
+                return AguaDoce;
+            }
+            set
+            {
+                AguaDoce = false;
+            }
+        }
+
+        public void Botar()
+        {
+            Console.WriteLine("Botou um ovo");
+        }
+
+        public void Chocar()
+        {
+            Console.WriteLine("Chocou um ovo, é um milagre");
+        }
+    }
+}
+
---
+++ N2_POO+ED/N2_POO+ED/Classes dos Animais/Tartaruga.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2_POO_ED.Classes_dos_Animais
+{
+    class Tartaruga
+    {
+    }
+}
